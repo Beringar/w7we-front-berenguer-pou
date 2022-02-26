c2a14978035b50f54cf9a210d347e7e19a37cf22
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-26 13:59:44 +0100

    ProfilePage boilerplate

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,10 @@
+const UsersPage = () => {
+  return (
+    <>
+      <main className="main">
+        <h1>USERS PAGE with a grid with all users...</h1>
+      </main>
+    </>
+  );
+};
+export default UsersPage;
