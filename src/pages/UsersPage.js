e4576bda9bf09b637a8d34f85c0010fc76494382
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-26 14:28:46 +0100

    added props to UsersPage myNetwork(boolean)

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,11 +1,12 @@
-const UsersPage = () => {
+const UsersPage = ({ myNetwork = false }) => {
   return (
     <>
       <main className="main">
         <h1>
-          USERS PAGE with a grid with: a) all users, b) all friends of logged in
+          {`USERS PAGE with a grid with: a) all users, b) all friends of logged in
           user c) all enemmies of logged in user (depending of props passed to
-          UsersList component)
+          UsersList component myNetwork=true/false, if myNetwork===false -> all
+          Users, else --> only users in user's network)`}
         </h1>
       </main>
     </>
