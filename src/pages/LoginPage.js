6f3e85387c18faac745251ae1517a073fc330cf7
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-26 13:53:09 +0100

    LoginPage boilerplate

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <>
+      <main className="main">
+        <h1>LOGIN PAGE with a login form...</h1>
+      </main>
+    </>
+  );
+};
+export default LoginPage;
